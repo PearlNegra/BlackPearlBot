f462a403ab6854fa43f3bb07beb5fc733bc157d0
PearlNegra <usernamejuntos@live.com>
2015-07-28 12:30:25 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1748,43 +1748,6 @@
                     }
                 }
             },
-			transCommand: {
-                command: 'trans',
-                rank: 'user',
-                type: 'startsWith',
-                transf: ['< olha quem tem buceta amor, fez na tailândia!',
-                ],
-                getTrans: function () {
-                    var c = Math.floor(Math.random() * this.transf.length);
-                    return this.transf[c];
-                },
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        var msg = chat.message;
-
-                        var space = msg.indexOf(' ');
-                        if (space === -1) {
-                            API.sendChat(basicBot.chat.trans);
-                            return false;
-                        }
-                        else {
-                            var name = msg.substring(space + 2);
-                            var user = basicBot.userUtilities.lookupUserName(name);
-                            if (user === false || !user.inRoom) {
-                                return API.sendChat(subChat(basicBot.chat.nousertrans, {name: name}));
-                            }
-                            else if (user.username === chat.un) {
-                                return API.sendChat(subChat(basicBot.chat.selftrans, {name: name}));
-                            }
-                            else {
-                                return API.sendChat(subChat(basicBot.chat.transd, {nameto: user.username, namefrom: chat.un, trans: this.getTrans()}));
-                            }
-                        }
-                    }
-                }
-            },
             cycleCommand: {
                 command: 'cycle',
                 rank: 'manager',
