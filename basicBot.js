7e4463aae3c188dcae8fda5d7bbcb8c6ee76c779
PearlNegra <usernamejuntos@live.com>
2015-07-30 00:33:38 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1750,6 +1750,52 @@
             },
             
             kissCommand: {
+				command: ['trans', 'trns'],
+				rank: 'user',
+				type: 'startsWith',
+				kisses: [
+				        "disse que você é muito garota, muito fina :information_desk_person:",
+				        "disse que você é muito garota, muito fina :information_desk_person:",
+				        "pediu pra você mostrar os peito de 10 mil.",
+				        "pediu pra você mostrar os peito de 10 mil.",
+				        "pediu pra você soltar a taba :smoking: , bicha!",
+				        "pediu pra você soltar a taba :smoking: , bicha!",
+				        "disse que você é quem tem buceta amor, fez na tailândia! Você sim domina a pista! :hocho:",
+				        "disse que você é quem tem buceta amor, fez na tailândia! Você sim domina a pista! :hocho:"
+				    ],
+				getKiss: function () {
+					var c = Math.floor(Math.random() * this.kisses.length);
+					return this.kisses[c];
+				},
+				functionality: function (chat, cmd) {
+					if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+					if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+					else {
+						var msg = chat.message;
+
+						var space = msg.indexOf(' ');
+						if (space === -1) {
+							API.sendChat(basicBot.chat.kiss);
+							return false;
+						}
+						else {
+							var name = msg.substring(space + 2);
+							var user = basicBot.userUtilities.lookupUserName(name);
+							if (user === false || !user.inRoom) {
+								return API.sendChat(subChat(basicBot.chat.nouserkiss, {name: name}));
+							}
+							else if (user.username === chat.un) {
+								return API.sendChat(subChat(basicBot.chat.selfkiss, {name: name}));
+							}
+							else {
+								return API.sendChat(subChat(basicBot.chat.kissed, {nameto: user.username, namefrom: chat.un, kiss: this.getKiss()}));
+							}
+						}
+					}
+				}
+			},
+
+            kissCommand: {
 				command: ['22', 'robot'],
 				rank: 'user',
 				type: 'startsWith',
